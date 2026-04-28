7af46ef0006ec63b79f398ae630c99935b158396
Naimur Rahman Nahid <Naimur.Rahman.Nahid@protonmail.com>
2026-04-28 20:51:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -484,4 +484,6 @@ else:
     return jsonify({"success": True, **data})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
